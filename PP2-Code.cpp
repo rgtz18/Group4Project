644f86e92b941ef8416ebdc1f671f5a2acff4eb2
Raul Gutierrez <87869231+rgtz18@users.noreply.github.com>
2021-11-05 16:29:02 -0500

    PP2-Code
    Added PP2-Code

---
+++ PP2-Code.cpp
@@ -0,0 +1,28 @@
+// ===================================================================================================================
+//  Project Name: Dunder Mifflin Database
+//  Developer: Group 4
+//  Date: November 4th, 2021
+//  Description: Company Database
+//  Version 1.0
+//
+//   Configuration Variables:
+//   
+//
+//   Development Team:
+//       - Gutierrez, Raul
+//       - Caldwell, Aaron
+//       - Delgado, Stepahnie
+//       - Escobedo, Jonathan
+//       - Kim, Arthur
+//       - Nguyen, Michael
+//
+//   Change Log:
+//           Version         User        Date            Changes
+// -------------------------------------------------------------------------------------------------------------------
+//           1.0             Rgtz18      11/04/2021      Created header for code, added develoment team names
+// ===================================================================================================================
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
