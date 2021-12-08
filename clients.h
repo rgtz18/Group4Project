d53cf8f33a744bd634e9805fc8e0756c990366b9
moonslimee <93737776+moonslimee@users.noreply.github.com>
2021-12-07 18:16:14 -0600

    Update clients.h

--- clients.h
+++ clients.h
@@ -5,25 +5,19 @@ using namespace std;
 class Clientlist
 {
 	public:
-		Clients(int saleID = 0, string enterDate = "None", int enterClientID = 0, int enterProductID = 0, double enterProductSales = 0.00, int enterSalesRep = 0);
-		void SetClientID(int enterClientID);
-		int  GetClientID ();
-		void SetDate(string enterDate);
-		string GetDate();
-		void SetClientID(int enterClientID);
-		int GetClientID();
-		void SetProductID(int enterProductID);
-		int  GetProductID ();
-		void SetProductSales(double enterProductSales);
-		double GetProductSales();
-		void SetClientID(int enterClientID);
-		int GetSalesRepID();
+	
+		void SetClientName(string revClientName);
+		void SetClientAddress(string revClientAddress);
+		void clientlist();
+		string GetClientName();
+		string GetClientAddress();
+		void newClient();
+		void monthlySalesList;
 		
 	private:
-		int saleID;
-		string date;
 		int clientID;
-		int productID;
+		string clientName;
+		string clientAddress;
 		double productSales;
-		int salesRepID;
+	
 };
